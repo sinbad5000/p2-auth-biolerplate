b62bef3740ed39d7a3aa70e04706c21f8b992f54
sinav <sinavameghi@gmail.com>
2020-06-29 14:24:59 -0700

    hlaf way

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const Express = require("express")
+const ejsLayouts = require("express-ejs-layouts")
+const helmet = require("helmet")
+const session = require("express-session")
+const flash = require("flash")
+
+
+
+const app = Express()
+app.use(Express.urlencoded({ extended: false}))
+app.use(Express.static(__dirname + "/public"))
+app.set("view engine", "ejs")
+app.use(ejsLayouts)
+app.use(require("morgan")("dev"))
+app.use(helmet())
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+
+app.listen(process.env.PORT || 3000)
---
+++ models/user.js
@@ -0,0 +1,57 @@
+"use strict"
+const bcrypt = require("bcrypt")
+
+
+module.exports = (sequelize, DataTypes) => {
+    //user object
+    const user = sequelize.define("user", {
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: {
+                    msg: "invalid email address"
+                }
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            validate: {
+                len: {
+                    args: [1,99],
+                    msg: "name bust be between 1-99 characters"
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            validate: {
+                args: [8, 99],
+                msg: "password is wrong length"
+            }
+        }
+    }, {
+        hooks: {
+            beforeCreate: function(createdUser, options) {
+                if (createdUser && createdUser.password) {
+                    let hash = bcrypt.hashSync(createdUser.password, 12)
+                    createdUser.password = hash 
+                }
+            }
+        }
+    })
+    user.associate = function(models) {
+        //any user associations
+    }  
+    //validate password def to validate password at user login
+    user.prototype.validPassword = function(passwordTyped) {
+        return bcrypt.compareSync(passwordTyped, this.password)
+    }
+    // remove password before any serialization of user object 
+    user.prototype.toJSON = function() {
+        let userData = this.get()
+        delete userData.password
+        return userData
+    }
+
+    return user;
+}
